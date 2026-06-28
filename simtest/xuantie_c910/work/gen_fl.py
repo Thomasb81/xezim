322c2d745f973580cdd999a7b735f6908ae69168
Bondan Rufen <bondanr@yahoo.com>
2026-06-28 14:48:18 -0700

    deleted work dir

--- simtest/xuantie_c910/work/gen_fl.py
+++
@@ -1,5 +0,0 @@
-import yaml, os, sys
-d = sys.argv[1]
-descr = yaml.safe_load(open(os.path.join(d, "descriptor.yaml")))
-for f in descr["compile"]["verilogSourceFiles"]:
-    print(os.path.join(d, f))
