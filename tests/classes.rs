f9889a20b1afa9e609c82388d29d853c23a24817
Bondan Rufen <bondanr@yahoo.com>
2026-08-05 15:42:58 -0700

    test: derived constraints on inherited properties in module-scope classes

--- tests/classes.rs
+++ tests/classes.rs
@@ -97,6 +97,8 @@ mod randomize_inside_range;
 mod scope_randomize_dist_and_foreach;
 #[path = "classes/class_unpacked_struct_properties.rs"]
 mod class_unpacked_struct_properties;
+#[path = "classes/module_scope_derived_constraints.rs"]
+mod module_scope_derived_constraints;
 #[path = "classes/super_property_access.rs"]
 mod super_property_access;
 #[path = "classes/subroutine_local_unpacked_structs.rs"]
