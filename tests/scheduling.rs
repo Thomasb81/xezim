236f886b412f4e6f73097932f63ef56e5e501ec2
Bondan Rufen <bondanr@yahoo.com>
2026-09-03 13:36:02 -0700

    Release inside settle re-drives the net

--- tests/scheduling.rs
+++ tests/scheduling.rs
@@ -207,3 +207,5 @@ mod delayed_write_pending_semantics;
 mod labeled_block_local_hier_ref;
 #[path = "scheduling/nba_wait_for_region_yield.rs"]
 mod nba_wait_for_region_yield;
+#[path = "scheduling/release_from_level_sensitive_block.rs"]
+mod release_from_level_sensitive_block;
